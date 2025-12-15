f68e52d75b396b1a1d39e5ae228dfef4c83b070e
trombley26-lab <trombley26@smsarcs.org>
2025-12-15 14:02:28 -0500

    changes

--- programs/codeFiles/8th/hailey.py
+++ programs/codeFiles/8th/hailey.py
@@ -3,8 +3,10 @@ import time
 #from lib.retro import run as retro_run
 #from lib.rainbowChase import run as rainbow_run
 #from lib.snowlights import run as snowlights
-#from lib.solid import run as solid 
+from lib.solid import run as solid 
 #from lib.twinkle import run as twinkle
 #actual code, stuff that will edit the tree
 def run(strip):
     rainbow_run(strip)
+    solid(strip(182,255,193))
+    time.sleep(45)
