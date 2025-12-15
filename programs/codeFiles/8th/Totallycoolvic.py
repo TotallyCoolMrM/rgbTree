f6d09c50f86f4aa75ab2062fee79d4689e3da39c
feeder26-ops <feeder26@smsarcs.org>
2025-12-15 14:02:59 -0500

    changes

--- programs/codeFiles/8th/Totallycoolvic.py
+++ programs/codeFiles/8th/Totallycoolvic.py
@@ -3,8 +3,10 @@ import time
 #from lib.retro import run as retro_run
 #from lib.rainbowChase import run as rainbow_run
 #from lib.snowlights import run as snowlights
-#from lib.solid import run as solid 
+from lib.solid import run as solid 
 #from lib.twinkle import run as twinkle
 #actual code, stuff that will edit the tree
 def run(strip):
     rainbow_run(strip)
+    solid(strip(137,252,172))
+    time.sleep(99)
