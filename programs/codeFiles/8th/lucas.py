12ee6a76ec392bfc72cb2fa7affedc62f1f64366
bray26-collab <bray26@smsarcs.org>
2025-12-15 14:08:34 -0500

    fixed import

--- programs/codeFiles/8th/lucas.py
+++ programs/codeFiles/8th/lucas.py
@@ -1,7 +1,7 @@
 #start of file, add all of your imports here #Uncomment the imports you want 
 import time
 #from lib.retro import run as retro_run
-#from lib.rainbowChase import run as rainbow_run
+from lib.rainbowChase import run as rainbow_run
 #from lib.snowlights import run as snowlights
 from lib.solid import run as solid 
 #from lib.twinkle import run as twinkle
