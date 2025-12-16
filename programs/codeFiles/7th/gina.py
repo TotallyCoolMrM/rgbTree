d0261b850bc4792cc9b4a700d8feab4d6da7755a
rgrabbe27-source <rgrabbe27@smsarcs.org>
2025-12-16 10:32:05 -0500

    regina grabbe

---
+++ programs/codeFiles/7th/gina.py
@@ -0,0 +1,11 @@
+#start of file, add all of your imports here #Uncomment the imports you want 
+import time
+#from lib.retro import run as retro_run
+from lib.rainbowChase import run as rainbow_run
+#from lib.solid import run as solid 
+#from lib.twinkle import run as twinkle
+#actual code, stuff that will edit the tree 
+def run(strip):
+    rainbow_run(strip)
+    time.sleep(3)
+    solid(strip,(26,50,55))
